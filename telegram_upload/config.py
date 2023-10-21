2b660738e9bb4e4868c0360ac33612935c319d71
Aik <36109582+Aikanairo@users.noreply.github.com>
2023-10-21 22:15:26 +0200

    Update config.py

--- telegram_upload/config.py
+++ telegram_upload/config.py
@@ -3,31 +3,22 @@ import os
 
 import click
 
-@click.command()
-@click.option('--config', default='~/.config', help='Specify the configuration directory')
-def main(config):
-    config_directory = os.path.expanduser(config)
-    config_file = os.path.expanduser(os.path.join(config_directory, 'telegram-upload.json'))
-    session_file = os.path.expanduser(os.path.join(config_directory, 'telegram-upload'))
+CONFIG_DIRECTORY = os.environ.get('TELEGRAM_UPLOAD_CONFIG_DIRECTORY', '/.config')
+CONFIG_FILE = os.path.expanduser('{}/telegram-upload.json'.format(CONFIG_DIRECTORY))
+SESSION_FILE = os.path.expanduser('{}/telegram-upload'.format(CONFIG_DIRECTORY))
 
-    def prompt_config(config_file):
-        os.makedirs(os.path.dirname(config_file), exist_ok=True)
-        click.echo('Go to https://my.telegram.org and create an App in API development tools')
-        api_id = click.prompt('Please Enter api_id', type=int)
-        api_hash = click.prompt('Now enter api_hash')
-        with open(config_file, 'w') as f:
-            json.dump({'api_id': api_id, 'api_hash': api_hash}, f)
-        return config_file
 
-    def default_config():
-        if os.path.lexists(config_file):
-            return config_file
-        return prompt_config(config_file)
+def prompt_config(config_file):
+    os.makedirs(os.path.dirname(config_file), exist_ok=True)
+    click.echo('Go to https://my.telegram.org and create a App in API development tools')
+    api_id = click.prompt('Please Enter api_id', type=int)
+    api_hash = click.prompt('Now enter api_hash')
+    with open(config_file, 'w') as f:
+        json.dump({'api_id': api_id, 'api_hash': api_hash}, f)
+    return config_file
 
-    config_file = default_config()
-    click.echo(f'Using config directory: {config_directory}')
-    click.echo(f'Using config file: {config_file}')
-    click.echo(f'Using session file: {session_file}')
 
-if __name__ == '__main__':
-    main()
+def default_config():
+    if os.path.lexists(CONFIG_FILE):
+        return CONFIG_FILE
+    return prompt_config(CONFIG_FILE)
